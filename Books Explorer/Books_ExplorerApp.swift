644a394d297ef067384a0120f727e84b601058bc
SinaVn <vosogh858sina@gmail.com>
2024-10-20 13:13:35 +0330

    Initial Commit

---
+++ Books Explorer/Books_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_ExplorerApp.swift
+//  Books Explorer
+//
+//  Created by Sina Vosough Nia on 7/29/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct Books_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
